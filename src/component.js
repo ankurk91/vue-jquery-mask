00a2c9b04562e5b71a8b4d7cf53d29a8fc55f020
ankurk91 <pro.ankurk1@gmail.com>
2018-11-14 22:44:21 +0530

    Convert template to render function

---
+++ src/component.js
@@ -0,0 +1,76 @@
+import jQuery from 'jquery';
+import 'jquery-mask-plugin';
+
+export default {
+  name: 'jquery-mask',
+  render(el) {
+    return el('input', {
+      attrs: {
+        type: 'text',
+      },
+      on: {
+        input: this.onInput,
+        blur: this.onBlur,
+      }
+    })
+  },
+  props: {
+    value: {
+      required: true,
+      default: null,
+      validator(value) {
+        return value === null || typeof value === 'string' || value instanceof String || typeof value === 'number'
+      }
+    },
+    mask: {
+      required: true,
+      type: [String, Function]
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+    // Set this prop to false to emit masked value
+    raw: {
+      type: Boolean,
+      default: true
+    },
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    // Clone configs
+    let newOptions = jQuery.extend(true, {}, this.options);
+    // Lastly init the mask
+    jQuery(this.$el).mask(this.mask, newOptions);
+    // Set initial value
+    jQuery(this.$el).val(jQuery(this.$el).masked(this.value));
+  },
+  methods: {
+    /**
+     * Update v-model upon change triggered by user
+     */
+    onInput(event) {
+      this.$nextTick(() => {
+        let toEmit = this.raw ? jQuery(this.$el).cleanVal() : event.target.value;
+        this.$emit('input', toEmit);
+      });
+    },
+    onBlur(event) {
+      this.$emit('blur', this.value)
+    }
+  },
+  watch: {
+    /**
+     * Listen to change from outside of component and update DOM
+     *
+     */
+    value(newValue) {
+      jQuery(this.$el).val(jQuery(this.$el).masked(newValue));
+    },
+  },
+  beforeDestroy() {
+    jQuery(this.$el).unmask();
+  }
+}
